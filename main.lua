681f2044e7406a4e56850955020fb8801cf7650d
aaravm95 <aaravamudhan@outlook.com>
2026-01-03 14:15:01 -0800

    Initial commit

---
+++ main.lua
@@ -0,0 +1,28 @@
+function myDraw(who, isSmart)
+    if isSmart then
+        love.graphics.print(who.." is SMART", 100, 200)
+    else
+        love.graphics.print(who.." is not SMART", 100, 200)
+    end
+end
+
+function setFontSize(fontSize)
+    love.graphics.setFont(love.graphics.newFont(fontSize))
+end
+  
+
+
+function love.draw()
+    setFontSize(50)    
+    love.graphics.print("Start")
+    setFontSize(60) 
+    myDraw("Aarav",true)
+    setFontSize(89)
+    love.graphics.print("end", 580,490)
+
+end
+
+
+
+
+
